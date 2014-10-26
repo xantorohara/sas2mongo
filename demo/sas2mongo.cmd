4d1f447ba16f2a073760ef35ce8e1ff851056b9a
Xantorohara <xantorohara@gmail.com>
2014-10-26 20:24:41 +0400

    moved jar file to target

--- demo/sas2mongo.cmd
+++ demo/sas2mongo.cmd
@@ -1,1 +1,1 @@
-java -jar sas2mongo.jar --file=sas-files/olympics.sas7bdat --collection=olympics --mongo-db demo --action=drop
+java -jar ../target/sas2mongo.jar --file=sas-files/olympics.sas7bdat --collection=olympics --mongo-db demo --action=drop
